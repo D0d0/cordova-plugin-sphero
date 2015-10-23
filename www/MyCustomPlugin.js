40b76f52a6f2dd50aec770ea496e195df083f03f
D0d0 <jozef.d13@gmail.com>
2015-10-23 12:50:47 +0200

    Initial commit

---
+++ www/MyCustomPlugin.js
@@ -0,0 +1,11 @@
+var MyCustomPlugin = {
+  PrintLog: function (successCallback, errorCallback, action) {
+    cordova.exec(
+            successCallback, // success callback function
+            errorCallback, // error callback function
+            'CustomPlugin', // mapped to our native Java class called
+            action, // with this action name , in this case 'beep'
+            []  )// arguments, if needed
+  }
+}
+module.exports = MyCustomPlugin;
