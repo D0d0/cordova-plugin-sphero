2f43c96727958619debb2c6b68a79ca01b933256
D0d0 <jozef.d13@gmail.com>
2015-10-23 23:44:23 +0200

    Test sphero connect

--- src/android/CustomPlugin.java
+++ src/android/CustomPlugin.java
@@ -19,17 +19,9 @@ import java.lang.Exception;
 import java.lang.Integer;
 import java.lang.String;
 
-import com.orbotix.ConvenienceRobot;
-import com.orbotix.DualStackDiscoveryAgent;
-import com.orbotix.common.DiscoveryException;
-import com.orbotix.common.Robot;
-import com.orbotix.common.RobotChangedStateListener;
-import com.orbotix.le.RobotLE;
-
 public class CustomPlugin extends CordovaPlugin {
 
     Integer a = 1;
-    private ConvenienceRobot mRobot;
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -38,30 +30,7 @@ public class CustomPlugin extends CordovaPlugin {
             a++;
             String err = "";
             try {
-                DualStackDiscoveryAgent.getInstance().addRobotStateListener(new RobotChangedStateListener() {
-                    @Override
-                    public void handleRobotChangedState(Robot robot, RobotChangedStateNotificationType type) {
-                        switch (type) {
-                            case Online:
-                            /*if (robot instanceof RobotLE) {
-                                ((RobotLE) robot).setDeveloperMode(true);
-                            }*/
-
-                                //Save the robot as a ConvenienceRobot for additional utility methods
-                                mRobot = new ConvenienceRobot(robot);
-
-                                mRobot.isConnected();
-
-                                mRobot.setLed(0.0f, 0.0f, 1.0f);
-                                mRobot.drive(90.0f, 5.0f);
-                                alert(a.toString(), err, "tu", callbackContext);
-                                callbackContext.success();
-                            case Disconnected:
-                                break;
-                        }
-                    }
-                });
-                DualStackDiscoveryAgent.getInstance().startDiscovery(cordova.getActivity().getApplicationContext());
+                new SpheroConnect(callbackContext, cordova.getActivity().getApplicationContext()).execute();
             } catch (Exception e) {
                 err = e.getMessage();
             }
---
+++ src/android/SpheroConnect.java
@@ -0,0 +1,52 @@
+package com.example.myplugin;
+
+import org.apache.cordova.CallbackContext;
+import android.os.AsyncTask;
+import android.content.Context;
+
+
+import com.orbotix.ConvenienceRobot;
+import com.orbotix.DualStackDiscoveryAgent;
+import com.orbotix.common.DiscoveryException;
+import com.orbotix.common.Robot;
+import com.orbotix.common.RobotChangedStateListener;
+import com.orbotix.le.RobotLE;
+
+public class SpheroConnect extends AsyncTask<String, String, String> {
+
+    private ConvenienceRobot mRobot;
+
+    private CallbackContext callbackContext;
+
+    public SpheroConnect(CallbackContext callbackContext,) {
+        this.callbackContext = callbackContext;
+        DualStackDiscoveryAgent.getInstance().startDiscovery(cordova.getActivity().getApplicationContext());
+    }
+
+    @Override
+    public String doInBackground(String... arg0) {
+        DualStackDiscoveryAgent.getInstance().addRobotStateListener(new RobotChangedStateListener() {
+            @Override
+            public void handleRobotChangedState(Robot robot, RobotChangedStateNotificationType type) {
+                switch (type) {
+                    case Online:
+                        if (robot instanceof RobotLE) {
+                            ((RobotLE) robot).setDeveloperMode(true);
+                        }
+
+                        //Save the robot as a ConvenienceRobot for additional utility methods
+                        mRobot = new ConvenienceRobot(robot);
+
+                        mRobot.isConnected();
+
+                        mRobot.setLed(0.0f, 0.0f, 1.0f);
+                        mRobot.drive(90.0f, 5.0f);
+                        this.callbackContext.success();
+                    case Disconnected:
+                        break;
+                }
+            }
+        });
+        return null;
+    }
+}
